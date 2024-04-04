5e7907e36c0a440f9c635efc351d0cfa4e17bfa4
shaellis <shaellis@udel.edu>
2024-04-03 21:31:12 -0400

    Added name "Shamus Ellis" to website for In Class: Final Project Setup

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          Shamus Ellis
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
