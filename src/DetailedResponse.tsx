dcd3f55ff14a5368cb133a285a9bc571aaac17b8
DylanBlev <DylanBlev2004@gmail.com>
2024-04-30 13:37:29 -0400

    removed "ProgressBar" because unused

--- src/DetailedResponse.tsx
+++ src/DetailedResponse.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, ProgressBar } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 export function DetailedResponse({
     question,
